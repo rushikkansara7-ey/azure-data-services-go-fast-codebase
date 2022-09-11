8862e3624548a704fc31a0054c6b5728c837b129
John Rampono <John.Rampono@microsoft.com>
2022-09-11 22:11:02 +0800

    downgrade to python 3.8

--- solution/DeploymentV2/utilities/GitHubRunnerInstall.sh
+++ solution/DeploymentV2/utilities/GitHubRunnerInstall.sh
@@ -29,6 +29,27 @@ sudo apt-get install figlet lolcat boxes
 sudo apt install python3-pip -y
 sudo apt install python3.10-venv -y
 
+#MSQL ODBC 18
+if ! [[ "18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
+then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
+    exit;
+fi
+
+sudo su
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+exit
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
 
 #Github Runner Software
 mkdir actions-runner && cd actions-runner# Download the latest runner package && \
